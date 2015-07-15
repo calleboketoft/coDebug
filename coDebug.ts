608e3923bb108ab954f7e3d02e1e6333758cb321
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-07-15 14:36:19 +0200

    preparing coDebug

---
+++ coDebug.ts
@@ -0,0 +1,18 @@
+// Disables console.debug if localStorage.debug
+
+export default angular.module('coDebugMode', [])
+
+.config(() => {
+
+  if (!(localStorage.debug)) { // eslint-disable-line no-undef
+    // noop the console.debug
+    console.debug = function () {}
+  } else {
+    // if the browser doesn't support console.debug
+    if (!console.debug) {
+      console.debug = function () {
+        console.log.apply(console, arguments)
+      }
+    }
+  }
+})
